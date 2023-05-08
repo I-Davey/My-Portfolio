70ee33a7efa1382c18c87f7ea6fa53214d5f5cce
cackl <idaveynz@gmail.com>
2023-05-08 14:49:30 +0400

    added vids, progressed with map
    videos were loaded into the folium_map folder, as i will remove dependency on backend so it can be a static website
    have managed to make the folium marker icons increase in size when i click on them. this is actually a very big achievement as there is very deep crosscommunication between the python and the javascript

--- src/routes/creations/folium_map/+page.svelte
+++ src/routes/creations/folium_map/+page.svelte
@@ -3,6 +3,7 @@
 <script>
   import { onMount } from "svelte";
   import axios from "axios";
+    import { beforeNavigate } from "$app/navigation";
 
   let isReady = false;
   let responseData;
@@ -11,9 +12,7 @@
     try {
       // Send a POST request to the specified API endpoint with the provided requestData
       const response = await axios.post(apiEndpoint, requestData);
-      console.log(response);
       const responseData = response.data;
-      console.log(responseData);
 
       return responseData;
     } catch (error) {
@@ -26,14 +25,91 @@
     // @ts-ignore
     await import("https://pyscript.net/latest/pyscript.js");
   }
+  /**
+   * Keeps track of the previously clicked marker.
+   */
+  let previousMarker;
+
+  /**
+   * Handles the click event on a marker.
+   *
+   * @param {Event} event - The click event.
+   */
+  function onMarkerClick(event) {
+    console.log("Marker clicked:", event);
+    console.log("previous:", previousMarker)
+    if (previousMarker) { scaleMarker(previousMarker, 2/3); }
+    const marker = event.sourceTarget;
+    scaleMarker(marker, 1.5);
+    previousMarker = marker;
+  }
+
+  /**
+   * Scales a marker according to the given scale factor.
+   *
+   * @param {Object} marker - The marker to be scaled.
+   * @param {number} scaleFactor - The scale factor.
+   */
+  function scaleMarker(marker, scaleFactor) {
+    const icon = marker._icon;
+    // Extract and parse the current styles.
+    const currentStyles = getStyles(icon);
+    // Scale the styles.
+    const scaledStyles = scaleStyles(currentStyles, scaleFactor);
+    // Update the styles.
+    setStyles(icon, scaledStyles);
+  }
+
+  /**
+   * Extracts and parses the current styles of the icon.
+   *
+   * @param {HTMLElement} icon - The icon element.
+   * @returns {Object} The current styles of the icon.
+   */
+  function getStyles(icon) {
+    const width = parseInt(icon.style.width.slice(0, -2));
+    const height = parseInt(icon.style.height.slice(0, -2));
+    const marginLeft = parseInt(icon.style.marginLeft.slice(0, -2));
+    const marginTop = parseInt(icon.style.marginTop.slice(0, -2));
+
+    return { width, height, marginLeft, marginTop };
+  }
+
+  /**
+   * Scales the styles according to the given scale factor.
+   *
+   * @param {Object} styles - The styles to be scaled.
+   * @param {number} scaleFactor - The scale factor.
+   * @returns {Object} The scaled styles.
+   */
+  function scaleStyles(styles, scaleFactor) {
+    return Object.fromEntries(
+      Object.entries(styles).map(([key, value]) => [key, Math.round(value * scaleFactor)])
+    );
+  }
+
+  /**
+   * Updates the styles of the icon with the given styles.
+   *
+   * @param {HTMLElement} icon - The icon element.
+   * @param {Object} styles - The styles to be applied.
+   */
+  function setStyles(icon, styles) {
+    icon.style.width = styles.width + "px";
+    icon.style.height = styles.height + "px";
+    icon.style.marginLeft = styles.marginLeft + "px";
+    icon.style.marginTop = styles.marginTop + "px";
+  }
+
 
   onMount(async () => {
     responseData = await fetchData("/api", { type: "gw_map" });
 
     if (responseData) {
       responseData = JSON.stringify(responseData);
-      window.responseData = responseData;
       window.fetchData = fetchData;
+      window.responseData = responseData;
+      window.onMarkerClick = onMarkerClick;
     }
 
     await initializePyScript();
@@ -56,6 +132,13 @@ import base64
 import io
 import pandas as pd
 import folium
+import branca
+#https://stackoverflow.com/questions/74707544/add-a-clickevent-function-to-multiple-folium-markers-with-python
+from jinja2 import Template
+from folium.map import Marker
+
+
+
 decoded = base64.b64decode(responseData)
 df = pd.read_csv(io.BytesIO(decoded))
 df['inbound'] = df['inbound'].astype(bool)
@@ -65,15 +148,46 @@ first_location = df.loc[0, "phsyical_coordinate"]
 first_lat, first_lon = map(float, first_location.strip("()").split(", "))
 m = folium.Map(location=[first_lat, first_lon], zoom_start=12)
 
+
+#in the html, run function onMarkerClick automatically
+
+click_template = """
+{'{'}% macro script(this, kwargs) %{'}'}
+var {'{{'} this.get_name() {'}}'} = L.marker(
+  {'{{'} this.location|tojson {'}}'},
+  {'{{'} this.options|tojson {'}}'}
+).addTo({'{{'} this._parent.get_name() {'}}'}).on('click', onClick);
+{'{'}% endmacro %{'}'}
+"""
+
+Marker._template = Template(click_template)
+click_js = """function onClick(e) {'{'}
+  //print parent 
+  self.parent.onMarkerClick(e);
+  {'}'}"""
+      
+e = folium.Element(click_js)
+html = m.get_root()
+html.script.get_root().render()
+html.script._children[e.get_name()] = e
+
 # Add markers for each location in the CSV df
 for index, row in df.iterrows():
+
+    
+    popup = folium.Popup(html, max_width=2650)
     coordinate_str = row["phsyical_coordinate"]
     lat, lon = map(float, coordinate_str.strip("()").split(", "))
-    folium.Marker([lat, lon], popup=row["loc_name"]).add_to(m)
+    #handle on marker click
+    x = folium.Marker([lat, lon])
+    x.add_to(m)
 
 display(m)
 </py-script>
 
+<div>
+  this is where image will beforeNavigate
+</div>
 </div>
 
 <style>
@@ -102,5 +216,8 @@ display(m)
         flex-wrap: nowrap;
         min-width: 100%;
     }
+    .leaflet-popup{
+        visibility: hidden;
+    }
     
-    </style>
+</style>
