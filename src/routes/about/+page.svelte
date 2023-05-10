f5f85c93870115e0c091bb49c675b88b8dd26eb8
cackl <idaveynz@gmail.com>
2023-05-10 13:00:11 +0400

    working on my design

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { setTitle } from "lib/scripts/store";
+    import { onDestroy, onMount } from "svelte";
   setTitle("About");
 
   const t1 = "Hi, I'm Ian Davey";
@@ -12,6 +13,38 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
   const image1 = "https://via.placeholder.com/400x300.png?text=Project+1";
   const image2 = "https://via.placeholder.com/400x300.png?text=Project+2";
   const image3 = "https://via.placeholder.com/400x300.png?text=Project+3";
+
+
+    let ismobile = false;
+
+    // check if screen size is mobile
+    function checkMobile() {
+        if (window.innerWidth <= 768) {
+            ismobile = true;
+        } else {
+            ismobile = false;
+        }
+    }
+
+    // call checkMobile on page load and resize
+    //if not window
+        //wait for window to load with promise
+
+    onMount(() => {
+
+        console.log('mounted');
+        checkMobile();
+        window.addEventListener('resize', checkMobile);
+    });
+
+    // remove event listener on page unload
+    onDestroy(() => {
+        //check if var window even exists
+         if (typeof window !== 'undefined') {
+                window.removeEventListener('resize', checkMobile);
+        }   
+        
+    });
 </script>
 
 
@@ -25,7 +58,7 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
 
     <div class="row">
       <div class="left">
-        <img src="https://via.placeholder.com/400x300.png?text=Project+1" alt="Project 1">
+        <img src="https://via.placeholder.com/400x300.png?text=Folium+Map" alt="Project 1">
       </div>
       <div class="right">
         <p>Ever since I started my journey in software development, Python has been my go-to programming language. Throughout my career, I have had the opportunity to work on diverse projects, which enabled me to sharpen my skills in Python application development, API integration, process automation, and web scraping. My aptitude for rapid learning and adaptability has empowered me to confront challenges directly and make valuable contributions to my teams.</p>
@@ -34,20 +67,32 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
     
     <div class="spacer"></div>
 
+    {#if ismobile}
     <div class="row">
       <div class="left">
-        <p>When I'm not coding, I love spending time with my cat and going to the beach. There's nothing more relaxing than feeling the sand between my toes and listening to the sound of the waves. My cat is my constant companion, and I enjoy playing with her and watching her explore the world around her. I'm also always learning and keeping up-to-date with the latest technologies and trends in the industry. I believe that staying informed and curious is key to being a successful developer, and I'm always looking for new ideas and inspiration to bring to my work.</p>
+        <img src="https://via.placeholder.com/400x300.png?text=Data+Visualisation+Gold" alt="Project 2">
       </div>
-      <div class="right">
-        <img src="https://via.placeholder.com/400x300.png?text=Project+2" alt="Project 2">
+    <div class="right">
+        <p>I'm always learning and keeping up-to-date with the latest technologies and trends in the industry. I believe that staying informed and curious is key to being a successful developer, and I'm always looking for new ideas and inspiration to bring to my work.</p>
       </div>
     </div>
+    {:else}
+        <div class="row">
+
+    <div class="left">
+        <p>I'm always learning and keeping up-to-date with the latest technologies and trends in the industry. I believe that staying informed and curious is key to being a successful developer, and I'm always looking for new ideas and inspiration to bring to my work.</p>
+      </div>
+    <div class="right">
+        <img src="https://via.placeholder.com/400x300.png?text=Data+Visualisation+Gold" alt="Project 2">
+      </div>
+    </div>
+    {/if}
 
     <div class="spacer"></div>
     
     <div class="row">
       <div class="left">
-        <img src="https://via.placeholder.com/400x300.png?text=Project+3" alt="Project 3">
+        <img src="https://via.placeholder.com/400x300.png?text=Telegram+Chat+Section" alt="Project 3">
       </div>
       <div class="right">
         <p>If you're looking for a skilled and passionate Python Developer to join your team, I'd love to hear from you. Please feel free to <a href='mailto:your-email@example.com'>contact me</a> to discuss how I can help your business grow.</p>
@@ -55,7 +100,6 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
     </div>
   </div>
 </div>
-
 <style>
   .about-container {
     background-color: #525252;
@@ -94,10 +138,12 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
 
   .left {
     width: 45%;
+    order: 1;
   }
 
   .right {
     width: 45%;
+    order: 2;
   }
 
   img {
@@ -105,23 +151,33 @@ const p2 = "When I'm not coding, I love spending time with my cat and going to t
     height: auto;
     margin-right: 1rem;
   }
-
-  @media (max-width: 768px) {
-    .row {
-      flex-direction: column;
-      align-items: flex-start;
-    }
-
-    .left,
-    .right {
-      width: 100%;
-    }
-
-    img {
-      margin-right: 0;
-      margin-bottom: 1rem;
-    }
+@media (max-width: 768px) {
+  .row {
+    flex-direction: column;
+    text-align: center;
   }
+
+  .left,
+  .right {
+    width: 100%;
+  }
+
+  img {
+    margin: 0 auto;
+    margin-bottom: 1rem;
+  }
+
+  .right p {
+    max-width: 100%;
+    width: 100%;
+    box-sizing: border-box;
+    padding-right: 1rem;
+    font-size: calc(0.75 * var(--img-height)); /* Set font-size to 75% of image height */
+    line-height: var(--img-height); /* Set line-height to image height */
+  }
+}
+
+
 
   .spacer {
     height: 4rem;
