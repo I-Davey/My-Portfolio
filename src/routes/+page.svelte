41bd7a6e2a102ef41401fb4dbc980cb92a5d3d66
cackl <idaveynz@gmail.com>
2023-05-11 10:25:16 +0400

    cleaned up the grammar and added replaced filler text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
     <div class="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"> Hi, I'm Ian. <br class="hidden lg:inline-block" /> Python Engineer & Innovator </h1>
-        <p class="mb-8 leading-relaxed"> I'm a passionate Python developer with extensive experience, eager to learn, grow, and add value to companies in the tech industry </p>
+        <p class="mb-8 leading-relaxed"> As a passionate Python developer with extensive experience, I am eager to learn, grow, and add value to companies in the tech industry.</p>
         <a href="#contact" class=" inline-flex text-black bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-400  hover:text-black rounded text-lg"> Contact Me </a>
       </div>
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
@@ -63,11 +63,11 @@
 
   </section>
 
-  <section id="projects" class="text-gray-400  body-font bg-gradient-to-b from-zinc-900  to-zinc-800" >
+  <section id="projects" class="body-font bg-gradient-to-b from-zinc-900  to-zinc-800" >
     <div class="container px-5 py-10 mx-auto text-center lg:px-40">
       <div class="flex flex-col w-full mb-20">
         <h1 class="sm:text-4xl text-3xl font-medium title-font mb-4 text-white"> Things I've Built </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam fuga dolore. </p>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Ever since I started my journey in software development, Python has been my go-to programming language. Throughout my career, I have had the opportunity to work on diverse projects, which enabled me to sharpen my skills in Python application development, API integration, process automation, and web scraping. My aptitude for rapid learning and adaptability has empowered me to confront challenges directly and make valuable contributions to my teams.</p>
       </div>
       <p></p> {#if outerWidth > 1000}
         
@@ -98,17 +98,15 @@
   <div class="spacer"></div>
 
 	</section>
-	<section id="skills" class="bg-gradient-to-b from-zinc-800 to-zinc-700">
+	<section id="skills" class="   bg-gradient-to-b from-zinc-800 to-zinc-700">
 		<div class="container px-5 py-10 mx-auto">
 			<div class="text-center mb-20">
 				<h1 class="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
           </h1>
-				<p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
-          </p>
+				<p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto ">
+          I continuously refine my skills and stay current with the latest industry technologies and trends, ensuring proficiency in various programming languages, frameworks, and tools. By leveraging advanced tools like ChatGPT and GitHub Copilot, I optimize my development process, streamline workflow, and deliver high-quality, innovative solutions. This combination of technical expertise and cutting-edge resources enables me to create impactful and future-proof projects.
+        </p>
 			</div>
 			<div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
 				<Skill skill="Python"/>
@@ -134,14 +132,14 @@
       <div class="spacer"></div>
   <div class="spacer"></div>
   </section>
-<section id="contact" class="text-gray-400 bg-zinc-700 body-font">
+<section id="contact" class="bg-zinc-700 body-font">
   <div class="container px-5 py-10 mx-auto text-center lg:px-40">
-    <div class="flex flex-col w-full mb-20">
+    <div class="flex flex-col w-full mb-10">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
         Contact Me
       </h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-        Let's get in touch! You can reach me through LinkedIn, GitHub, or email.
+        Let's get in touch! You can reach me through LinkedIn, GitHub, or email:
       </p>
     </div>
     <div class="flex justify-center flex-wrap md:flex-nowrap">
@@ -155,7 +153,7 @@
         Email
       </a>
     </div>
-    <p class="lg:w-2/3 mx-auto leading-relaxed text-base pt-20 pb-5">
+    <p class="lg:w-2/3 mx-auto leading-relaxed text-base pt-10 pb-5">
       Or check out my other stuff:
     </p>
     <div class="flex flex-col w-full mb-20">
